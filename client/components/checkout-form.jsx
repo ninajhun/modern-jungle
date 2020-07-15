c6e2a0c049766a3d9b2b26df397d4d7be9955d76
Nina Jhunjhnuwala <ninajhun@berkeley.edu>
2020-07-15 21:02:49 +0000

    add checkout form inputs

--- client/components/checkout-form.jsx
+++ client/components/checkout-form.jsx
@@ -1,7 +1,42 @@
 import React from 'react';
 
-function CheckoutForm(props) {
-  return <h1>hiii</h1>;
+class CheckoutForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: '',
+      creditCard: '',
+      address: ''
+    };
+  }
+
+  render() {
+    return (
+      <div className="row">
+        <form className="col-12" onSubmit ={this.handleSubmit}>
+
+          <div className="form-group">
+            <label>Name</label>
+            <input type='text' className="form-control" value={this.state.value} onChange ={this.handleChange} />
+          </div>
+
+          <div className="form-group">
+            <label>Credit Card</label>
+            <input type='text' className="form-control" value={this.state.value} onChange={this.handleChange} />
+          </div>
+
+          <div className="form-group">
+            <label>Address</label>
+            <textarea className="form-control" value={this.state.value} onChange={this.handleChange}></textarea>
+          </div>
+
+        </form>
+
+      </div>
+
+    );
+
+  }
 
 }
 
