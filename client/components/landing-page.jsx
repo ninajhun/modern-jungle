dec3d0b59edb8524327668a5437942ab0633eba7
Nina Jhunjhnuwala <ninajhun@berkeley.edu>
2020-08-06 04:23:35 +0000

    add html text of landing-page modal

--- client/components/landing-page.jsx
+++ client/components/landing-page.jsx
@@ -1,7 +1,15 @@
 import React from 'react';
 
 function LandingPage(props) {
-  return null;
+  return (
+    <div>
+      <p className="welcome-text">welcome to </p>
+      <p className="title-text">Urban Jungle</p>
+      <p className="title-description">This website is for demonstration purposes only. </p>
+      <p>No real purchases will be made. </p>
+      <p className="enter-button">ENTER</p>
+    </div>
+  );
 }
 
 export default LandingPage;
