08ffaf4bbb1e8736201494b4e78b9e25be405266
Nina Jhunjhnuwala <ninajhun@berkeley.edu>
2020-08-06 04:08:27 +0000

    fixed when number of items in cart = 1 to show item instead of items

--- client/components/header.jsx
+++ client/components/header.jsx
@@ -16,9 +16,7 @@ class Header extends React.Component {
 
   render() {
     let items;
-    this.props.cartItemCount === '1'
-      ? items = 'Item'
-      : items = 'Items';
+    this.props.cartItemCount === 1 ? items = 'Item' : items = 'Items';
 
     return (
       <div className="row header py-2 align-items-center">
